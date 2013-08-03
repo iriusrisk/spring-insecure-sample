a1ba086374b967b5cce255b7cc768ce12ab27a7d
continuumsecurity <stephen@continuumsecurity.net>
2013-08-03 16:06:25 +0200

    initial

---
+++ src/main/java/net/continuumsecurity/dao/SearchException.java
@@ -0,0 +1,13 @@
+package net.continuumsecurity.dao;
+
+/**
+ *
+ * @author jgarcia
+ */
+public class SearchException extends RuntimeException {
+
+    public SearchException(Throwable ex) {
+        super(ex);
+    }
+
+}
